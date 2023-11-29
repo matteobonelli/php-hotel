dea31bce582dfe8b03b24e36fd38b3e243f4cc74
matteobonelli <matteobonelli08@gmail.com>
2023-11-29 15:31:29 +0100

    added table to display hotels

--- index.php
+++ index.php
@@ -1,19 +1,59 @@
-<?php include __DIR__ . '/Model/db.php'; ?>
+<?php
+include __DIR__ . '/partials/header.php';
 
-<?php foreach ($hotels as $hotel) { ?>
-    <div>Nome:
-        <?php echo $hotel['name'] ?>
-    </div>
-    <div>Descrizione:
-        <?php echo $hotel['description'] ?>
-    </div>
-    <div>Parcheggio:
-        <?php echo $hotel['parking'] ?>
-    </div>
-    <div>Voto:
-        <?php echo $hotel['vote'] ?>
-    </div>
-    <div>Distanza dal centro:
-        <?php echo $hotel['distance_to_center'] ?>
-    </div>
-<?php } ?>
+$counter = 0;
+?>
+<table class="table">
+    <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Nome</th>
+            <th scope="col">Descrizione</th>
+            <th scope="col">Parcheggio</th>
+            <th scope="col">Voto</th>
+            <th scope="col">Distanza dal Centro</th>
+        </tr>
+    </thead>
+    <tbody>
+        <?php foreach ($hotels as $hotel) { ?>
+            <tr>
+                <th scope="row">
+                    <?php echo $counter += 1 ?>
+                </th>
+                <td>
+                    <?php echo $hotel['name'] ?>
+                </td>
+                <td>
+                    <?php echo $hotel['description'] ?>
+                </td>
+                <td>
+                    <?php if ($hotel['parking']) {
+                        echo 'Si';
+                    } else {
+                        echo 'No';
+                    } ?>
+                </td>
+                <td>
+                    <?php echo $hotel['vote'] ?>
+                </td>
+                <td>
+                    <?php echo $hotel['distance_to_center'] . ' km' ?>
+                </td>
+            </tr>
+        <?php } ?>
+
+    </tbody>
+</table>
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+    crossorigin="anonymous"></script>
+
+</body>
+
+</html>
---
+++ partials/header.php
@@ -0,0 +1,20 @@
+<?php
+include __DIR__ . '/../Model/db.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>PHP Hotel</title>
+</head>
+
+<body class="container">
+    <header class="mt-4 mb-5">
+        <h1>Migliori Hotel di Zona</h1>
+    </header>
